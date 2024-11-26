bf3b791e9fdb516e1d5549dece04dab1422f4359
vitorv7519 <vitorestradas@hotmail.com>
2024-11-26 16:28:02 -0300

    new map tip and label for null elements on materials and setting layer as not editable

--- ui/qgisred_thematicmaps_dialog.py
+++ ui/qgisred_thematicmaps_dialog.py
@@ -8,10 +8,12 @@ from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QDialog, QMessageBox, QWidget
 from PyQt5 import sip
 from qgis.PyQt import uic
+from qgis.PyQt.QtCore import QVariant
 
 # QGIS imports
 from qgis.core import QgsLayerTreeGroup, QgsLayerTreeLayer, QgsLayerTreeNode, QgsProject
 from qgis.core import QgsVectorFileWriter, QgsVectorLayer
+from qgis.core import QgsProject, QgsVectorLayer, QgsPalLayerSettings, QgsVectorLayerSimpleLabeling, QgsTextFormat
 from qgis.utils import iface
 
 # Local imports
@@ -150,19 +152,20 @@ class QGISRedThematicMapsDialog(QDialog, FORM_CLASS):
         
         if field == 'Material':
             QGISRedUtils().apply_categorized_renderer(derived_layer, field, qml_path)
+            #self.set_labels_with_null_handling(derived_layer, field, qml_path)
 
         QgsProject.instance().addMapLayer(derived_layer, False)
         QGISRedUtils().hide_fields(derived_layer, field)
-        
+
         if parent_group and not sip.isdeleted(parent_group):
             layer_tree_layer = parent_group.insertLayer(layer_position, derived_layer)
             layer_tree_layer.setCustomProperty("showFeatureCount", True)
-            
+        
         main_layer.dataChanged.connect(lambda: self.sync_layers(main_layer, derived_layer))
         main_layer.styleChanged.connect(lambda: self.sync_layers(main_layer, derived_layer))
         derived_layer.dataChanged.connect(lambda: derived_layer.triggerRepaint())
         derived_layer.setReadOnly(True)
-        
+
         return derived_layer
 
     def find_layer_recursively(self, parent_group, layer_name):
@@ -238,6 +241,29 @@ class QGISRedThematicMapsDialog(QDialog, FORM_CLASS):
             new_renderer = main_layer.renderer().clone()
             derived_layer.setRenderer(new_renderer)
             derived_layer.triggerRepaint()
+
+    def set_labels_with_null_handling(self, layer, field_name, qml_file_path): 
+        if not layer or not isinstance(layer, QgsVectorLayer):
+            return
+
+        # Create the label expression using coalesce to handle NULL values
+        expression = f"""
+                    CASE
+                        WHEN "{field_name}" IS NULL THEN '#NA'
+                        ELSE "{field_name}"
+                    END
+        """
+        # Set up label settings
+        label_settings = QgsPalLayerSettings()
+        label_settings.fieldName = expression
+        label_settings.isExpression = True
+        label_settings.placement= QgsPalLayerSettings.OverPoint
+        # Apply labeling to the layer
+        labeling = QgsVectorLayerSimpleLabeling(label_settings)
+        layer.setLabeling(labeling)
+
+        layer.triggerRepaint()
+    
 
     def get_selected_queries(self):
         units = QGISRedUtils().getUnits()
