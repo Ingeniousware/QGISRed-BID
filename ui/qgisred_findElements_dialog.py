1d76997bd2987c429c436a64917e0b6203c13653
vitorv7519 <vitorestradas@hotmail.com>
2024-11-30 14:13:47 -0300

    label with name of element

--- ui/qgisred_findElements_dialog.py
+++ ui/qgisred_findElements_dialog.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 
 import os
-
-from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QDialog, QMessageBox
+from PyQt5.QtGui import QIcon, QFont
+from PyQt5.QtWidgets import QDialog, QMessageBox, QWidget
+from PyQt5 import sip
 from qgis.PyQt import uic
-
-from qgis.core import QgsProject
-
+from qgis.PyQt.QtCore import QVariant, Qt
+from PyQt5.QtCore import pyqtSlot
+from qgis.core import (
+    QgsLayerTreeGroup, QgsLayerTreeLayer, QgsLayerTreeNode, QgsProject,
+    QgsVectorFileWriter, QgsVectorLayer,
+    QgsPalLayerSettings, QgsVectorLayerSimpleLabeling, QgsTextFormat
+)
 from qgis.utils import iface
 
 from ..tools.qgisred_utils import QGISRedUtils
@@ -16,7 +20,6 @@ FORM_CLASS, _ = uic.loadUiType(os.path.join(os.path.dirname(__file__), "qgisred_
 
 class QGISRedFindElementsDialog(QDialog, FORM_CLASS):
     def __init__(self, parent=None):
-        """Constructor."""
         super(QGISRedFindElementsDialog, self).__init__(parent)
         self.setupUi(self)
         self.setDialogStyle()
@@ -34,23 +37,60 @@ class QGISRedFindElementsDialog(QDialog, FORM_CLASS):
             "Isolation Valves"
         ]
         
+        self.singular_forms = {
+            "Reservoirs": "Reservoir",
+            "Tanks": "Tank",
+            "Junctions": "Junction",
+            "Pumps": "Pump",
+            "Valves": "Valve",
+            "Pipes": "Pipe",
+            "Meters": "Meter",
+            "Service Connections": "Service Connection",
+            "Isolation Valves": "Isolation Valve"
+        }
+        
         self.original_ids = []
+        
+        font = QFont()
+        font.setPointSize(12)
+        font.setBold(True)
+        self.labelFoundElement.setFont(font)
         
         self.setupConnections()
         self.initializeElementTypes()
-    
-    def setDialogStyle(self):
-        icon_path = os.path.join(os.path.dirname(__file__), '..', 'images', 'iconFindElements.png')
-        self.setWindowIcon(QIcon(icon_path))
-
+        self.labelFoundElement.setText("")
+        
+        current_id = self.cbElementId.currentText()
+        if current_id:
+            self.updateFoundElementLabel(current_id)
+        
     def setupConnections(self):
         self.cbElementType.currentIndexChanged.connect(self.updateElementIds)
         self.leElementMask.textChanged.connect(self.filterElementIds)
         self.btFind.clicked.connect(self.findElement)
+        self.cbElementId.currentTextChanged.connect(self.updateFoundElementLabel)
         
     def initializeElementTypes(self):
         self.cbElementType.clear()
         self.cbElementType.addItems(self.element_types)
+        
+    def updateFoundElementLabel(self, selected_id):
+        if not selected_id:
+            self.labelFoundElement.setText("")
+            return
+            
+        element_type = self.cbElementType.currentText()
+        if element_type == "All Elements":
+            for layer_type, singular in self.singular_forms.items():
+                layer = self.getLayerForElementType(layer_type)
+                if layer:
+                    for feature in layer.getFeatures():
+                        if str(feature.attribute("Id")) == selected_id:
+                            self.labelFoundElement.setText(f"{singular} {selected_id}")
+                            return
+        else:
+            singular = self.singular_forms.get(element_type, element_type)
+            self.labelFoundElement.setText(f"{singular} {selected_id}")
         
     def getLayerForElementType(self, element_type):
         project = QgsProject.instance()
@@ -75,9 +115,11 @@ class QGISRedFindElementsDialog(QDialog, FORM_CLASS):
             return layers[0] if layers else None
         return None
         
+    @pyqtSlot()
     def updateElementIds(self):
         self.cbElementId.clear()
         self.original_ids.clear()
+        self.labelFoundElement.setText("")
         
         selected_type = self.cbElementType.currentText()
         if selected_type == "All Elements":
@@ -96,9 +138,9 @@ class QGISRedFindElementsDialog(QDialog, FORM_CLASS):
         else:
             self.cbElementId.addItems(self.original_ids)
                 
+    @pyqtSlot()
     def filterElementIds(self):
         mask = self.leElementMask.text().strip()
-        
         self.cbElementId.clear()
         
         if mask:
@@ -108,8 +150,8 @@ class QGISRedFindElementsDialog(QDialog, FORM_CLASS):
             
         self.cbElementId.addItems(filtered_items)
         
+    @pyqtSlot()
     def findElement(self):
-        print("HERE")
         selected_type = self.cbElementType.currentText()
         selected_id = self.cbElementId.currentText()
         
@@ -117,10 +159,29 @@ class QGISRedFindElementsDialog(QDialog, FORM_CLASS):
             QMessageBox.warning(self, "Warning", "Please select an element ID")
             return
             
-        layer = self.getLayerForElementType(selected_type)
-        if layer:
-            for feature in layer.getFeatures():
-                if str(feature.attribute("Id")) == selected_id:
-                    iface.mapCanvas().zoomToFeatureIds(layer, [feature.id()])
-                    layer.selectByIds([feature.id()])
+        found = False
+        if selected_type == "All Elements":
+            for element_type in self.element_types[1:]:
+                layer = self.getLayerForElementType(element_type)
+                if layer:
+                    for feature in layer.getFeatures():
+                        if str(feature.attribute("Id")) == selected_id:
+                            iface.mapCanvas().zoomToFeatureIds(layer, [feature.id()])
+                            layer.selectByIds([feature.id()])
+                            found = True
+                            break
+                if found:
                     break
+        else:
+            layer = self.getLayerForElementType(selected_type)
+            if layer:
+                for feature in layer.getFeatures():
+                    if str(feature.attribute("Id")) == selected_id:
+                        iface.mapCanvas().zoomToFeatureIds(layer, [feature.id()])
+                        layer.selectByIds([feature.id()])
+                        found = True
+                        break
+                    
+    def setDialogStyle(self):
+        icon_path = os.path.join(os.path.dirname(__file__), '..', 'images', 'iconFindElements.png')
+        self.setWindowIcon(QIcon(icon_path))
