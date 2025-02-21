522a13d698ca1ebf60f83872d2314e62ee2b0b58
vitorv7519 <vitorestradas@hotmail.com>
2025-02-20 23:41:50 -0300

    double click finds element underneath

--- tools/qgisred_identifyFeature.py
+++ tools/qgisred_identifyFeature.py
@@ -4,7 +4,6 @@ from qgis.utils import iface
 from qgis.core import QgsProject, QgsVectorLayer
 from PyQt5.QtCore import Qt
 
-
 class QGISRedIdentifyFeature(QgsMapToolIdentify):
     def __init__(self, canvas, toggle_action=None):
         super().__init__(canvas)
@@ -12,8 +11,96 @@ class QGISRedIdentifyFeature(QgsMapToolIdentify):
         self.toggle_action = toggle_action
         self.currentHighlight = None
         self.dock = None
+        self.ignoreNextRelease = False
         self.setupConnections()
 
+    # -----------------------
+    # Helper Methods
+    # -----------------------
+    def getHandlers(self):
+        return {
+            'qgisred_meters': (['tabData', 'tabResults', 'tabCurves', 'tabControls'], 'handleMeters'),
+            'qgisred_isolationvalves': (['tabData', 'tabResults', 'tabCurves', 'tabControls'], 'handleIsolationValves'),
+            'qgisred_junctions': (['tabData', 'tabResults', 'tabPatterns', 'tabControls'], 'handleJunctions'),
+            'qgisred_valves': (['tabData', 'tabResults', 'tabCurves', 'tabControls'], 'handleValves'),
+            'qgisred_pumps': (['tabData', 'tabResults', 'tabCurves', 'tabPatterns', 'tabControls'], 'handlePumps'),
+            'qgisred_tanks': (['tabData', 'tabResults', 'tabCurves', 'tabPatterns', 'tabControls'], 'handleTanks'),
+            'qgisred_reservoirs': (['tabData', 'tabResults', 'tabPatterns', 'tabControls'], 'handleReservoirs'),
+            'qgisred_pipes': (['tabData', 'tabResults', 'tabCurves', 'tabControls'], 'handlePipes'),
+            'qgisred_serviceconnections': (['tabData', 'tabResults', 'tabCurves', 'tabControls'], 'handleServiceConnections')
+        }
+
+    def clearSelections(self):
+        for lyr in QgsProject.instance().mapLayers().values():
+            if isinstance(lyr, QgsVectorLayer):
+                lyr.removeSelection()
+
+    def highlightFeature(self, layer, feature):
+        self.clearHighlights()
+        self.currentHighlight = QgsHighlight(self.canvas, feature.geometry(), layer)
+        self.currentHighlight.setColor(Qt.red)
+        self.currentHighlight.setWidth(4)
+        self.currentHighlight.setFillColor(Qt.transparent)
+        self.currentHighlight.show()
+
+    def showFeatureInDock(self, layer, feature, handler=None):
+        self.dock = QGISRedElementsPropertyDock.getInstance(iface.mainWindow())
+        if not self.dock.isVisible():
+            iface.addDockWidget(Qt.RightDockWidgetArea, self.dock)
+        if self.dock.findElemetsdock:
+            self.dock.findElemetsdock.findFeature(layer, feature)
+        if handler:
+            tabs, method_name = handler
+            getattr(self.dock, method_name)(layer, feature, tabs)
+        else:
+            self.dock.loadFeature(layer, feature)
+        self.dock.show()
+        self.dock.raise_()
+        self.dock.activateWindow()
+
+    def selectFeature(self, layer, feature):
+        layer.select(feature.id())
+
+    def getFeatureByPriority(self, all_features):
+        handlers = self.getHandlers()
+        selected_feature = None
+        selected_layer = None
+        selected_handler = None
+
+        for identifier_key, handler_data in handlers.items():
+            for result in all_features:
+                identifier = result.mLayer.customProperty("qgisred_identifier")
+                if identifier == identifier_key:
+                    selected_feature = result.mFeature
+                    selected_layer = result.mLayer
+                    selected_handler = handler_data
+                    break
+            if selected_handler:
+                break
+
+        if not selected_handler and all_features:
+            top_result = all_features[0]
+            selected_feature = top_result.mFeature
+            selected_layer = top_result.mLayer
+
+        return selected_layer, selected_feature, selected_handler
+
+    def getSortedFeatures(self, all_features):
+        handlers = self.getHandlers()
+        sorted_results = []
+        for result in all_features:
+            identifier = result.mLayer.customProperty("qgisred_identifier")
+            if identifier in handlers:
+                priority = list(handlers.keys()).index(identifier)
+            else:
+                priority = len(handlers)
+            sorted_results.append((priority, result))
+        sorted_results.sort(key=lambda x: x[0])
+        return sorted_results
+
+    # -----------------------
+    # Additional Methods
+    # -----------------------
     def clearHighlights(self):
         if self.currentHighlight is not None:
             self.currentHighlight.hide()
@@ -43,71 +130,48 @@ class QGISRedIdentifyFeature(QgsMapToolIdentify):
         project.readProject.connect(self.deactivate)
         project.cleared.connect(self.deactivate)
 
+    # -----------------------
+    # Event Handlers
+    # -----------------------
     def canvasReleaseEvent(self, event):
+        if self.ignoreNextRelease:
+            self.ignoreNextRelease = False
+            return
+
         all_features = self.identify(event.x(), event.y(), self.TopDownAll)
         if not all_features:
             return
 
-        handlers = {
-            'qgisred_meters': (['tabData', 'tabResults', 'tabCurves', 'tabControls'], 'handleMeters'),
-            'qgisred_isolationvalves': (['tabData', 'tabResults', 'tabCurves', 'tabControls'], 'handleIsolationValves'),
-            'qgisred_junctions': (['tabData', 'tabResults', 'tabPatterns', 'tabControls'], 'handleJunctions'),
-            'qgisred_valves': (['tabData', 'tabResults', 'tabCurves', 'tabControls'], 'handleValves'),
-            'qgisred_pumps': (['tabData', 'tabResults', 'tabCurves', 'tabPatterns', 'tabControls'], 'handlePumps'),
-            'qgisred_tanks': (['tabData', 'tabResults', 'tabCurves', 'tabPatterns', 'tabControls'], 'handleTanks'),
-            'qgisred_reservoirs': (['tabData', 'tabResults', 'tabPatterns', 'tabControls'], 'handleReservoirs'),
-            'qgisred_pipes': (['tabData', 'tabResults', 'tabCurves', 'tabControls'], 'handlePipes'),
-            'qgisred_serviceconnections': (['tabData', 'tabResults', 'tabCurves', 'tabControls'], 'handleServiceConnections')
-        }
+        selected_layer, selected_feature, selected_handler = self.getFeatureByPriority(all_features)
+        self.clearSelections()
+        self.selectFeature(selected_layer, selected_feature)
+        self.highlightFeature(selected_layer, selected_feature)
+        self.showFeatureInDock(selected_layer, selected_feature, selected_handler)
 
-        selected_feature = None
-        selected_layer = None
-        selected_handler = None
+    def canvasDoubleClickEvent(self, event):
+        self.ignoreNextRelease = True
 
-        for identifier_key, handler_data in handlers.items():
-            for result in all_features:
-                identifier = result.mLayer.customProperty("qgisred_identifier")
-                if identifier == identifier_key:
-                    selected_feature = result.mFeature
-                    selected_layer = result.mLayer
-                    selected_handler = handler_data
-                    break 
-            if selected_handler:
-                break
+        all_features = self.identify(event.x(), event.y(), self.TopDownAll)
+        if not all_features:
+            return
 
-        if not selected_handler:
-            top_result = all_features[0]
-            selected_feature = top_result.mFeature
-            selected_layer = top_result.mLayer
+        sorted_results = self.getSortedFeatures(all_features)
+        if len(sorted_results) < 2:
+            self.canvasReleaseEvent(event)
+            return
 
-        for lyr in QgsProject.instance().mapLayers().values():
-            if isinstance(lyr, QgsVectorLayer):
-                lyr.removeSelection()
-        selected_layer.select(selected_feature.id())
+        second_result = sorted_results[1][1]
+        selected_feature = second_result.mFeature
+        selected_layer = second_result.mLayer
 
-        self.clearHighlights()
-        self.currentHighlight = QgsHighlight(self.canvas, selected_feature.geometry(), selected_layer)
-        self.currentHighlight.setColor(Qt.red)
-        self.currentHighlight.setWidth(4)
-        self.currentHighlight.setFillColor(Qt.transparent)
-        self.currentHighlight.show()
+        handlers = self.getHandlers()
+        identifier = selected_layer.customProperty("qgisred_identifier")
+        selected_handler = handlers.get(identifier, None)
 
-        self.dock = QGISRedElementsPropertyDock.getInstance(iface.mainWindow())
-        if not self.dock.isVisible():
-            iface.addDockWidget(Qt.RightDockWidgetArea, self.dock)
-
-        if self.dock.findElemetsdock:
-            self.dock.findElemetsdock.findFeature(selected_layer, selected_feature)
-
-        if selected_handler:
-            tabs, method_name = selected_handler
-            getattr(self.dock, method_name)(selected_layer, selected_feature, tabs)
-        else:
-            self.dock.loadFeature(selected_layer, selected_feature)
-
-        self.dock.show()
-        self.dock.raise_()
-        self.dock.activateWindow()
+        self.clearSelections()
+        self.selectFeature(selected_layer, selected_feature)
+        self.highlightFeature(selected_layer, selected_feature)
+        self.showFeatureInDock(selected_layer, selected_feature, selected_handler)
 
     def deactivate(self):
         self.canvas.unsetMapTool(self.canvas.mapTool())
