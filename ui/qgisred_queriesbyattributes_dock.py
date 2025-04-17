bd549ce747d6a370733b4018bddfa3cb882683ba
Vitor Vieira <131308846+vitorv7519@users.noreply.github.com>
2025-04-17 01:46:41 -0300

    queries by attributes draft

---
+++ ui/qgisred_queriesbyattributes_dock.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from PyQt5.QtWidgets import QDockWidget
+from PyQt5.QtCore import Qt
+from qgis.PyQt import uic
+from ..tools.qgisred_utils import QGISRedUtils
+import os
+
+
+FORM_CLASS, _ = uic.loadUiType(os.path.join(os.path.dirname(__file__), "qgisred_queriesbyattributes_dock.ui"))
+
+class QGISRedQueriesByAttributesDock(QDockWidget, FORM_CLASS):
+    def __init__(self, iface, parent=None):
+        super(QGISRedQueriesByAttributesDock, self).__init__(parent or iface.mainWindow())
+        self.setupUi(self) 
+        self.iface = iface
+        self.canvas = iface.mapCanvas()
