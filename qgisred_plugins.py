2ccf9656f7285879f37cde2caf1a5f965d083196
vitorv7519 <vitorestradas@hotmail.com>
2025-07-25 10:51:23 -0300

    fix project qlr

--- qgisred_plugins.py
+++ qgisred_plugins.py
@@ -1839,7 +1839,7 @@ class QGISRed:
         utils.removeLayers(self.ownFiles, ".dbf")
         utils.removeLayers(self.especificComplementaryLayers)
         utils.removeLayers(self.issuesLayers)
-        utils.removeTopLevelGroups(["Inputs", "Queries", "Results"])
+        #utils.removeTopLevelGroups(["Inputs", "Queries", "Results"])
         if task is not None:
             return {"task": task.definition()}
 
@@ -1936,7 +1936,7 @@ class QGISRed:
 
         # Always remove the one project‑level QLR file if it was created
         utils.deleteProjectQLR()
-
+        
         # Continue any pending task
         if task is not None:
             return {"task": task.definition()}
