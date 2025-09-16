9a4e27e38ffb75e7d6632cf8d9c6f3bc994c8ea6
Vitor Vieira <131308846+vitorv7519@users.noreply.github.com>
2025-09-16 13:15:50 -0300

    halt setCulture on plugin

--- qgisred_plugins.py
+++ qgisred_plugins.py
@@ -290,9 +290,9 @@ class QGISRed:
         self.removingLayers = False        
 
 
-        ui_language = QgsApplication.locale()
-        definedCulture = GISRed.SetCulture(ui_language)
-        QgsMessageLog.logMessage("Culture set to " + definedCulture, "QGISRed", level=0)
+        # ui_language = QgsApplication.locale()
+        # definedCulture = GISRed.SetCulture(ui_language)
+        # QgsMessageLog.logMessage("Culture set to " + definedCulture, "QGISRed", level=0)
 
         QgsMessageLog.logMessage(self.tr("Loaded sucssesfully"), "QGISRed", level=0)
 
