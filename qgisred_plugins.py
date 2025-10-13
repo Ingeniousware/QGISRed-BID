ba94ce7ac9ec6f3d32b1096a40c7fd9407022591
Vitor Vieira <131308846+vitorv7519@users.noreply.github.com>
2025-10-13 08:05:52 -0300

    Add data by import after Summary

--- qgisred_plugins.py
+++ qgisred_plugins.py
@@ -461,6 +461,17 @@ class QGISRed:
             add_to_toolbar=True,
             parent=self.iface.mainWindow(),
         )
+        icon_path = ":/plugins/QGISRed-BID/images/iconAddData.png"
+        self.add_action(
+            icon_path,
+            text=self.tr("Add data by import"),
+            callback=self.runCanAddData,
+            menubar=self.projectMenu,
+            toolbar=self.projectToolbar,
+            actionBase=projectDropButton,
+            add_to_toolbar=True,
+            parent=self.iface.mainWindow(),
+        )
         icon_path = ":/plugins/QGISRed-BID/images/iconLayerManagement.png"
         self.add_action(
             icon_path,
@@ -477,17 +488,6 @@ class QGISRed:
             icon_path,
             text=self.tr("Edit Legends"),
             callback=self.runLegends,
-            menubar=self.projectMenu,
-            toolbar=self.projectToolbar,
-            actionBase=projectDropButton,
-            add_to_toolbar=True,
-            parent=self.iface.mainWindow(),
-        )
-        icon_path = ":/plugins/QGISRed-BID/images/iconAddData.png"
-        self.add_action(
-            icon_path,
-            text=self.tr("Add data by import"),
-            callback=self.runCanAddData,
             menubar=self.projectMenu,
             toolbar=self.projectToolbar,
             actionBase=projectDropButton,
