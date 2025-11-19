3d955be6b72fa9cf0fb134353c8b5a4e97681c04
vitorv7519 <vitorestradas@hotmail.com>
2025-11-19 08:29:10 -0600

    WindowStaysOnTopHint

--- ui/qgisred_legends_dialog.py
+++ ui/qgisred_legends_dialog.py
@@ -73,7 +73,7 @@ class QGISRedLegendsDialog(QDialog, formClass):
         """Configure window appearance and custom title bar."""
         iconPath = os.path.join(os.path.dirname(__file__), '..', 'images', 'iconThematicMaps.png')
         self.setWindowIcon(QIcon(iconPath))
-        self.setWindowFlags(Qt.Window | Qt.FramelessWindowHint)
+        self.setWindowFlags(Qt.Window | Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint)
         self.setupCustomTitleBar()
         self.btClassPlus.setIcon(QIcon(":/images/themes/default/symbologyAdd.svg"))
         self.btClassMinus.setIcon(QIcon(":/images/themes/default/symbologyRemove.svg"))
