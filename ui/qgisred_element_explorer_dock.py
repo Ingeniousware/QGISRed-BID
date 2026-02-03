13bc70921296753a90e66cdf7daf1df8b5ea6f7a
Vitor Vieira <131308846+vitorv7519@users.noreply.github.com>
2026-02-03 08:45:45 -0300

    - when units change

--- ui/qgisred_element_explorer_dock.py
+++ ui/qgisred_element_explorer_dock.py
@@ -967,7 +967,7 @@ class QGISRedElementExplorerDock(QDockWidget, FORM_CLASS):
 
             # Get unit for the field with special handling for roughness
             fieldUnit = self.getFieldUnitWithHeadlossLogic(utils, layerIdentifier, fieldName, headloss, unitSystem)
-            unitItem = QTableWidgetItem(fieldUnit)
+            unitItem = QTableWidgetItem(fieldUnit if fieldUnit else "-")
             unitItem.setTextAlignment(Qt.AlignCenter)
             # Add tooltip with full unit name
             unitFullName = self.getFieldUnitFullNameWithHeadlossLogic(utils, layerIdentifier, fieldName, headloss, unitSystem)
