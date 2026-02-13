356ca0b51ad51cf28bb545ceebba39bbba6ec1a6
Nestor Lerma <aguilerma@gmail.com>
2026-02-13 08:28:00 +0100

    Name and Dialog Adjustments

--- tools/qgisred_createPipe.py
+++ tools/qgisred_createPipe.py
@@ -1,7 +1,14 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QColor
-from qgis.core import QgsPointXY, QgsPoint, QgsGeometry, QgsProject, QgsSnappingConfig, QgsTolerance, Qgis
+from qgis.core import QgsPointXY, QgsPoint, QgsGeometry, QgsProject, QgsSnappingConfig, QgsTolerance
 from qgis.gui import QgsMapTool, QgsVertexMarker, QgsRubberBand, QgsMapCanvasSnappingUtils
+try:
+    from qgis.gui import Qgis
+except:
+    try:
+        from qgis.core import Qgis # Compatibility with QGis 3.4x
+    except:
+        pass
 
 
 class QGISRedCreatePipeTool(QgsMapTool):
@@ -82,17 +89,12 @@ class QGISRedCreatePipeTool(QgsMapTool):
         for p in points:
             myPoints1.append(QgsPoint(p.x(), p.y()))
         myPoints1.remove(myPoints1[-1])
-
         if self.rubberBand1 is not None:
             self.iface.mapCanvas().scene().removeItem(self.rubberBand1)
-            
-        if Qgis.QGIS_VERSION_INT >= 33000:
-            # QGIS 3.30 or newer
+        try:  # From QGis 3.30
             self.rubberBand1 = QgsRubberBand(self.iface.mapCanvas(), Qgis.GeometryType.Line)
-        else:
-            # Older QGIS versions (< 3.30) only accept bool (False for line)
+        except:
             self.rubberBand1 = QgsRubberBand(self.iface.mapCanvas(), False)
-
         self.rubberBand1.setToGeometry(QgsGeometry.fromPolyline(myPoints1), None)
         self.rubberBand1.setColor(QColor(240, 40, 40))
         self.rubberBand1.setWidth(1)
